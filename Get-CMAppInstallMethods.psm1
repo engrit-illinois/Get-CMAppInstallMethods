a95c5838c6dcc2a07c62727defdcf87260586e9b
Matt Seng <mmseng@users.noreply.github.com>
2025-07-22 18:24:46 -0500

    Initial commit. Fully functional.

---
+++ Get-CMAppInstallMethods.psm1
@@ -0,0 +1,137 @@
+function Get-CMAppInstallMethods {
+	param(
+		[Parameter(Position=0,Mandatory=$true)]
+		[string]$Name,
+		
+		[switch]$ReturnAppsInsteadOfDeploymentTypes,
+		
+		[int]$Verbosity = 0,
+		
+		[string]$SiteCode="MP0",
+		
+		[string]$Provider="sccmcas.ad.uillinois.edu",
+		
+		[string]$CMPSModulePath="$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+	)
+	
+	function log {
+		param(
+			[string]$msg,
+			[int]$L,
+			[int]$V = 0
+		)
+		for($i = 0; $i -lt $L; $i += 1) {
+			$msg = "    $msg"
+		}
+		$ts = Get-Date -Format "HH:mm:ss:ffff"
+		if($V -le $Verbosity) {
+			Write-Host "[$ts] $msg"
+		}
+	}
+	
+	function count($array) {
+		$count = 0
+		if($array) {
+			# If we didn't check $array in the above if statement, this would return 1 if $array was $null
+			# i.e. @().count = 0, @($null).count = 1
+			$count = @($array).count
+			# We can't simply do $array.count, because if it's null, that would throw an error due to trying to access a method on a null object
+		}
+		$count
+	}
+	
+	function addm($property, $value, $object, $adObject = $false) {
+		# Shorthand for an annoying common line
+		
+		if($adObject) {
+			# This gets me EVERY FLIPPIN TIME:
+			# https://stackoverflow.com/questions/32919541/why-does-add-member-think-every-possible-property-already-exists-on-a-microsoft
+			$object | Add-Member -NotePropertyName $property -NotePropertyValue $value -Force
+		}
+		else {
+			$object | Add-Member -NotePropertyName $property -NotePropertyValue $value
+		}
+		$object
+	}
+	
+	function Prep-MECM {
+		log "Preparing connection to MECM..."
+		$initParams = @{}
+		if((Get-Module ConfigurationManager) -eq $null) {
+			# The ConfigurationManager Powershell module switched filepaths at some point around CB 18##
+			# So you may need to modify this to match your local environment
+			Import-Module $CMPSModulePath @initParams -Scope Global
+		}
+		if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+			New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $Provider @initParams
+		}
+		Set-Location "$($SiteCode):\" @initParams
+		log "Done prepping connection to MECM." -L 1
+	}
+	
+	function Get-Apps {
+		log "Getting apps..."
+		$apps = Get-CMApplication -Fast -Name $Name
+		$appsCount = count $apps
+		log "Found $appsCount matching apps." -L 1
+		$apps
+	}
+	
+	function Get-AppDts($apps) {
+		log "Getting app deployment types..."
+		$apps = $apps | ForEach-Object {
+			$app = $_
+			log $app.LocalizedDisplayName -L 1 -V 1
+			$dts = $app | Get-CMDeploymentType
+			$dtsCount = count $dts
+			log "Found $dtsCount deployment types." -L 2 -V 2
+			$dts = $dts | ForEach-Object {
+				$dt = $_
+				$xml = $dt | Select -ExpandProperty "SDMPackageXML"
+				$xmlData = [xml]$xml
+				$dt = addm "XmlData" $xmlData $dt
+				$dt
+			}
+			$app = addm "DeploymentTypes" $dts $app
+			$app
+		}
+		$apps
+	}
+	
+	function Get-FlattenedData($apps) {
+		log "Flattening data into array of deployment types..."
+		$apps | ForEach-Object {
+			$app = $_
+			$app.DeploymentTypes | ForEach-Object {
+				$dt = $_
+				[PSCustomObject]@{
+					Application = $app.LocalizedDisplayName
+					DeploymentType = $dt.LocalizedDisplayName
+					InstallMethod = $dt.XmlData.AppMgmtDigest.DeploymentType.Installer.InstallAction.Args.Arg | Where { $_.Name -eq "InstallCommandLine" } | Select -ExpandProperty "#text"
+					Uninstallmethod = $dt.XmlData.AppMgmtDigest.DeploymentType.Installer.UninstallAction.Args.Arg | Where { $_.Name -eq "InstallCommandLine" } | Select -ExpandProperty "#text"
+				}
+			}
+		} | Select Application,DeploymentType,InstallMethod,UninstallMethod | Sort Application
+	}
+	
+	function Do-Stuff {
+		$myPWD = $pwd.path
+		Prep-Mecm
+		
+		$apps = Get-Apps
+		$apps = Get-AppDts $apps
+		$dts = Get-FlattenedData $apps
+		
+		if($ReturnAppsInsteadOfDeploymentTypes) {
+			$apps
+		}
+		else {
+			$dts
+		}
+		
+		Set-Location $myPWD
+	}
+	
+	Do-Stuff
+	log "EOF"
+}
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Get-CMAppInstallMethods
+# Summary
+
 Module to pull the "Install program" and "Uninstall program" fields from all deployment types of all matching MECM applications
+
+# Example usage
+
+1. Download `Get-CMAppInstallMethods.psm1` to the appropriate subdirectory of your PowerShell [modules directory](https://github.com/engrit-illinois/how-to-install-a-custom-powershell-module).
+2. Run it, using the documentation provided below.
+
+# Parameters
+
+### -Name
+Required string.  
+A wildcard query string against which to match applications.  
+Only matching applications are polled and returned.  
+
+### -ReturnAppsInsteadOfDeploymentTypes
+Optional switch.  
+Normally the module returns a flattened array of deployment types and their associated apps and install/uninstall methods.  
+When specified, an array of applications and all the gathered deployment type info retrieved is returned instead.  
+
+### -Verbosity
+Optional integer.  
+Default is `0`.  
+Higher values cause logging to be more and more verbose.  
+
+### -SiteCode
+Optional string. Recommend leaving default.  
+The site code of the MECM site to connect to.  
+Default is `MP0`.  
+
+### -Provider
+Optional string. Recommend leaving default.  
+The SMS provider machine name.  
+Default is `sccmcas.ad.uillinois.edu`.  
+
+### -CMPSModulePath
+Optional string. Recommend leaving default.  
+The path to the ConfigurationManager Powershell module installed on the local machine (as part of the admin console).  
+Default is `$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1`.  
+
+# Notes
+- By mseng3. See my other projects here: https://github.com/mmseng/code-compendium.
